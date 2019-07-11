65e7ac7710aa32be3062a3a18bdff0afaf2fb5c1
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-07-11 15:36:57 +0900

    style: Remove name for unused value

--- src/stdlib/object.rs
+++ src/stdlib/object.rs
@@ -16,7 +16,7 @@ pub fn create_methods() -> Vec<SkMethod> {
     create_method("Object", "putchar(ord: Int) -> Void", |code_gen, function| {
         let n = function.get_params()[1].into_int_value();
         let func = code_gen.module.get_function("putchar").unwrap();
-        code_gen.builder.build_call(func, &[n.as_basic_value_enum()], "putchar");
+        code_gen.builder.build_call(func, &[n.as_basic_value_enum()], "");
         code_gen.builder.build_return(None);
         Ok(())
     }),
