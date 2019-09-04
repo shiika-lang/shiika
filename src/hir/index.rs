cda9dcd02693ef59d8a6ce144299a8194fd395d7
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-09-04 19:06:33 +0900

    refactor: Changed index_xx to be method of Index

--- src/hir/index.rs
+++ src/hir/index.rs
@@ -20,12 +20,10 @@ type IndexBody = HashMap<ClassFullname, HashMap<MethodName, MethodSignature>>;
 
 impl Index {
     pub fn new(stdlib: &Stdlib, toplevel_defs: &Vec<ast::Definition>) -> Result<Index, Error> {
-        let mut body = HashMap::new();
-
-        index_stdlib(&mut body, stdlib);
-        index_program(&mut body, toplevel_defs)?;
-
-        Ok(Index { body })
+        let mut index = Index { body: HashMap::new() };
+        index.index_stdlib(stdlib);
+        index.index_program(toplevel_defs)?;
+        Ok(index)
     }
 
     /// Find a method from class name and first name
@@ -37,55 +35,55 @@ impl Index {
     pub fn class_exists(&self, class_fullname: &str) -> bool {
         self.body.contains_key(&ClassFullname(class_fullname.to_string()))
     }
-}
 
-fn index_stdlib(body: &mut IndexBody, stdlib: &Stdlib) {
-    stdlib.sk_classes.values().for_each(|sk_class| {
-        let mut sk_methods = HashMap::new();
-        sk_class.method_sigs.iter().for_each(|sig| {
-            sk_methods.insert(sig.name.clone(), sig.clone());
+    fn index_stdlib(&mut self, stdlib: &Stdlib) {
+        stdlib.sk_classes.values().for_each(|sk_class| {
+            let mut sk_methods = HashMap::new();
+            sk_class.method_sigs.iter().for_each(|sig| {
+                sk_methods.insert(sig.name.clone(), sig.clone());
+            });
+            self.body.insert(sk_class.fullname.clone(), sk_methods);
         });
-        body.insert(sk_class.fullname.clone(), sk_methods);
-    });
-}
+    }
 
-fn index_program(body: &mut IndexBody, toplevel_defs: &Vec<ast::Definition>) -> Result<(), Error> {
-    toplevel_defs.iter().try_for_each(|def| {
-        match def {
-            ast::Definition::ClassDefinition { name, defs } => {
-                index_class(body, &name, &defs);
-                Ok(())
-            },
-            _ => {
-                Err(error::syntax_error(&format!("must not be toplevel: {:?}", def)))
+    fn index_program(&mut self, toplevel_defs: &Vec<ast::Definition>) -> Result<(), Error> {
+        toplevel_defs.iter().try_for_each(|def| {
+            match def {
+                ast::Definition::ClassDefinition { name, defs } => {
+                    self.index_class(&name, &defs);
+                    Ok(())
+                },
+                _ => {
+                    Err(error::syntax_error(&format!("must not be toplevel: {:?}", def)))
+                }
             }
-        }
-    })
-}
+        })
+    }
 
-fn index_class(body: &mut IndexBody, name: &ClassName, defs: &Vec<ast::Definition>) {
-    let class_fullname = name.to_class_fullname(); // TODO: nested class
-    let instance_ty = ty::raw(&class_fullname.0);
-    let class_ty = instance_ty.meta_ty();
+    fn index_class(&mut self, name: &ClassName, defs: &Vec<ast::Definition>) {
+        let class_fullname = name.to_class_fullname(); // TODO: nested class
+        let instance_ty = ty::raw(&class_fullname.0);
+        let class_ty = instance_ty.meta_ty();
 
-    let metaclass_fullname = class_ty.fullname;
-    let mut instance_methods = HashMap::new();
-    let mut class_methods = HashMap::new();
+        let metaclass_fullname = class_ty.fullname;
+        let mut instance_methods = HashMap::new();
+        let mut class_methods = HashMap::new();
 
-    defs.iter().for_each(|def| {
-        match def {
-            ast::Definition::InstanceMethodDefinition { sig, .. } => {
-                let hir_sig = crate::hir::create_signature(class_fullname.to_string(), sig);
-                instance_methods.insert(sig.name.clone(), hir_sig);
-            },
-            ast::Definition::ClassMethodDefinition { sig, .. } => {
-                let hir_sig = crate::hir::create_signature(metaclass_fullname.to_string(), sig);
-                class_methods.insert(sig.name.clone(), hir_sig);
-            },
-            _ => panic!("TODO")
-        }
-    });
+        defs.iter().for_each(|def| {
+            match def {
+                ast::Definition::InstanceMethodDefinition { sig, .. } => {
+                    let hir_sig = crate::hir::create_signature(class_fullname.to_string(), sig);
+                    instance_methods.insert(sig.name.clone(), hir_sig);
+                },
+                ast::Definition::ClassMethodDefinition { sig, .. } => {
+                    let hir_sig = crate::hir::create_signature(metaclass_fullname.to_string(), sig);
+                    class_methods.insert(sig.name.clone(), hir_sig);
+                },
+                _ => panic!("TODO")
+            }
+        });
 
-    body.insert(class_fullname, instance_methods);
-    body.insert(metaclass_fullname, class_methods);
+        self.body.insert(class_fullname, instance_methods);
+        self.body.insert(metaclass_fullname, class_methods);
+    }
 }
