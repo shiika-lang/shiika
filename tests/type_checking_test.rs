982d5c46530879092bc1bdb7e27c527b16276e03
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-10-13 19:20:22 +0900

    test: Fix broken test

--- tests/type_checking_test.rs
+++ tests/type_checking_test.rs
@@ -1,4 +1,5 @@
 use shiika::hir::*;
+use shiika::names::*;
 use shiika::ty;
 use shiika::stdlib::Stdlib;
 
@@ -13,7 +14,8 @@ fn test_discarding_return_value() -> Result<(), Box<dyn std::error::Error>> {
     ";
     let ast = shiika::parser::Parser::parse(src)?;
     let hir = shiika::hir::Hir::from_ast(ast, Stdlib::empty())?;
-    let method = &hir.sk_methods.values().next().unwrap()[0];
+    println!("sk_methods: {:?}", hir.sk_methods);
+    let method = &hir.sk_methods.get(&ClassFullname("A".to_string())).unwrap()[0];
     assert_eq!(method.signature.ret_ty, ty::raw("Void"));
     assert_eq!(method.body, SkMethodBody::ShiikaMethodBody {
         exprs: HirExpressions {
