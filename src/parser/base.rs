335863f7d9cdccc7ac3e2c1de1c51336e2a3b061
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-10-29 20:37:44 +0900

    refactor: Add comment

--- src/parser/base.rs
+++ src/parser/base.rs
@@ -85,6 +85,7 @@ impl<'a> Parser<'a> {
     }
 
     /// Return next token which is not `Token::Space`
+    /// Note: newlines are not skipped. (i.e. this function may return Token::Newline)
     pub (in super) fn next_nonspace_token(&mut self) -> Token {
         if self.current_token_is(Token::Space) {
             self.lexer.peek_next()
