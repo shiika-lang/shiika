889e2bed00a074c25e4def61908818fdbbc4a8be
Yutaka HARA <yutaka.hara+github@gmail.com>
2019-11-25 16:13:57 +0900

    feat(cli): shiika compile

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
+use std::fs;
+#[macro_use]
+extern crate clap;
 use shiika;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let str = "
-class A
-  def fib(n: Int) -> Int
-    if n < 3
-      1
-    else
-      fib(n-1) + fib(n-2)
-    end
-  end
-end
-a = A.new
-putchar a.fib(40)
-putchar 20
-putchar 79
-putchar 107";
-    let ast = shiika::parser::Parser::parse(str)?;
-    let stdlib = shiika::stdlib::Stdlib::create();
-    let hir = shiika::hir::Hir::from_ast(ast, stdlib)?;
-    let mut code_gen = shiika::code_gen::CodeGen::new();
-    code_gen.gen_program(hir)?;
-    code_gen.module.print_to_file("a.ll")?;
+    let yaml = load_yaml!("cli.yml");
+    let matches = clap::App::from(yaml).get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("compile") {
+        let filepath = matches.value_of("INPUT").unwrap();
+        let str = fs::read_to_string(filepath)?;
+        let ast = shiika::parser::Parser::parse(&str)?;
+        let stdlib = shiika::stdlib::Stdlib::create();
+        let hir = shiika::hir::Hir::from_ast(ast, stdlib)?;
+        let mut code_gen = shiika::code_gen::CodeGen::new();
+        code_gen.gen_program(hir)?;
+        code_gen.module.print_to_file(filepath.to_string() + ".ll")?;
+    }
+
     Ok(())
 }
