c3341c8278c27625975e9f7ed9bf547bdeb35b12
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-05-12 23:25:12 +0900

    test: Write integraiton test in Shiika

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,12 +1,23 @@
+use std::fs;
 use std::process::Command;
+use std::path::Path;
 
 #[test]
 fn test_compile_and_run() -> Result<(), Box<dyn std::error::Error>> {
-    let src = "
-class A; def foo; end; end
-putchar 72
-putchar 100 + 5";
-    let ast = shiika::parser::Parser::parse(src)?;
+    let paths = fs::read_dir("tests/sk/")?;
+    for item in paths {
+        run_sk_test(&item.unwrap().path())?;
+    }
+    Ok(())
+}
+
+/// Execute tests/sk/x.sk
+/// Fail if it prints something
+fn run_sk_test(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+        dbg!(&path);
+    let src = fs::read_to_string(path)?;
+
+    let ast = shiika::parser::Parser::parse(&src)?;
     let corelib = shiika::corelib::Corelib::create();
     let hir = shiika::hir::build(ast, corelib)?;
     let mut code_gen = shiika::code_gen::CodeGen::new(&hir);
@@ -30,7 +41,7 @@ putchar 100 + 5";
     let stdout = String::from_utf8(output.stdout).expect("invalid utf8 in stdout");
     let stderr = String::from_utf8(output.stderr).expect("invalid utf8 in stderr");
     assert_eq!(stderr, "");
-    assert_eq!(stdout, "Hi");
+    assert_eq!(stdout, "");
     Ok(())
 }
 
