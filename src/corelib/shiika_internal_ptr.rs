7a9ba86270fde2eb1efa7e8a4de3bf78507a56f7
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-05-14 22:47:26 +0900

    update: as_ -> into_

--- src/corelib/shiika_internal_ptr.rs
+++ src/corelib/shiika_internal_ptr.rs
@@ -9,7 +9,7 @@ pub fn create_methods() -> Vec<SkMethod> {
         let ptr = function.get_params()[0];
         let n_bytes = function.get_params()[1];
         let newptr = unsafe {
-            code_gen.builder.build_gep(*ptr.as_pointer_value(), &[*n_bytes.as_int_value()], "newptr")
+            code_gen.builder.build_gep(ptr.into_pointer_value(), &[n_bytes.into_int_value()], "newptr")
         };
         code_gen.builder.build_return(Some(&newptr));
         Ok(())
