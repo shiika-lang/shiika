335075c1e4a851555c293742fcde94943424c95e
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-07-07 00:42:59 +0900

    fix: Should skip non-.sk files

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,18 +1,23 @@
 use std::fs;
-use std::path::Path;
+use shiika::error::*;
 
 #[test]
 fn test_compile_and_run() -> Result<(), Box<dyn std::error::Error>> {
     let paths = fs::read_dir("tests/sk/")?;
     for item in paths {
-        run_sk_test(&item.unwrap().path())?;
+        let pathbuf = item?.path();
+        let path = pathbuf.to_str()
+            .ok_or(plain_runner_error("Filename not utf8"))?;
+        if path.ends_with(".sk") {
+            run_sk_test(path)?;
+        }
     }
     Ok(())
 }
 
 /// Execute tests/sk/x.sk
 /// Fail if it prints something
-fn run_sk_test(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+fn run_sk_test(path: &str) -> Result<(), Box<dyn std::error::Error>> {
     dbg!(&path);
     shiika::runner::compile(path)?;
     let (stdout, stderr) = shiika::runner::run(path)?;
