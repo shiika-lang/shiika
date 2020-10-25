d620affe3842b71fe18e9c5abde0b8cc1f1d48d3
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-10-25 14:06:59 +0900

    feat(mir): Support override

--- src/mir/vtable.rs
+++ src/mir/vtable.rs
@@ -4,35 +4,55 @@ use crate::names::*;
 use crate::ty::*;
 use crate::hir::sk_class::SkClass;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct VTable {
-    indices: HashMap<MethodFullname, usize>,
+    /// List of methods, ordered by index
+    fullnames: Vec<MethodFullname>,
+    /// Mapping from firstname to index
+    index: HashMap<MethodFirstname, usize>,
 }
 
 impl VTable {
     /// Create an empty VTable
     pub fn null() -> VTable {
-        VTable { indices: HashMap::new() }
-    }
-
-    /// Build a VTable
-    pub fn build(super_vtable: &VTable, class: &SkClass) -> VTable {
-        let mut indices = super_vtable.indices.clone();
-        let mut i = indices.len();
-
-        let method_names = class.method_sigs.values().map(|x| x.fullname.clone());
-        for name in method_names {
-            indices.insert(name, i);
-            i += 1;
+        VTable { 
+            fullnames: vec![],
+            index: HashMap::new(),
         }
-        VTable { indices }
     }
 
-    /// Returns the list of methods, ordered by the index.
-    pub fn to_vec(&self) -> Vec<MethodFullname> {
-        let mut v = self.indices.iter().collect::<Vec<_>>();
-        v.sort_unstable_by_key(|(_, i)| i.clone());
-        v.into_iter().map(|(name, _)| name.clone()).collect()
+    /// Build a VTable of a class
+    pub fn build(super_vtable: &VTable, class: &SkClass) -> VTable {
+        let mut vtable = super_vtable.clone();
+        for name in class.method_names() {
+            if vtable.contains(&name.first_name) {
+                vtable.update(name);
+            }
+            else {
+                vtable.push(name);
+            }
+        }
+        vtable
+    }
+
+    fn contains(&self, name: &MethodFirstname) -> bool {
+        self.index.contains_key(name)
+    }
+
+    fn update(&mut self, name: MethodFullname) {
+        let i = self.index.get(&name.first_name).unwrap();
+        self.fullnames.insert(*i, name);
+    }
+
+    fn push(&mut self, name: MethodFullname) {
+        let i = self.fullnames.len();
+        self.index.insert(name.first_name.clone(), i);
+        self.fullnames.push(name);
+    }
+
+    /// Returns the list of method names, ordered by the index.
+    pub fn to_vec(&self) -> &Vec<MethodFullname> {
+        &self.fullnames
     }
 }
 
