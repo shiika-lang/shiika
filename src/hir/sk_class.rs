7c5b629050e773edd55d50083f39ebc5af2e8d41
Yutaka HARA <yutaka.hara+github@gmail.com>
2020-12-13 20:14:34 +0900

    feat(hir): specialized_meta()

--- src/hir/sk_class.rs
+++ src/hir/sk_class.rs
@@ -1,4 +1,5 @@
 use crate::names::*;
+use crate::ty;
 use crate::ty::*;
 use std::collections::HashMap;
 
@@ -30,5 +31,29 @@ impl SkClass {
         // Sort by first name
         v.sort_unstable_by(|a, b| a.first_name.0.cmp(&b.first_name.0));
         v
+    }
+
+    /// Create a specialized metaclass of a generic metaclass
+    /// eg. create `Meta:Array<Int>` from `Meta:Array`
+    pub fn specialized_meta(&self, tyargs: &[TermTy]) -> SkClass {
+        // `self` must be a generic metaclass.
+        debug_assert!(self.typarams.len() > 0);
+        let base_name =
+            if let TyBody::TyMeta { base_fullname } = &self.instance_ty.body { base_fullname }
+            else { panic!("SkClass::specialize: not TyMeta") };
+        let instance_ty = ty::spe_meta(&base_name, tyargs.to_vec());
+        let method_sigs = self.method_sigs.iter().map(|(name, sig)| {
+            (name.clone(), sig.specialize(tyargs))
+        }).collect(); //::<Vec<_>>;
+
+        SkClass {
+            fullname: instance_ty.fullname.clone(),
+            typarams: vec![],
+            superclass_fullname: self.superclass_fullname.clone(),
+            instance_ty,
+            ivars: self.ivars.clone(),
+            method_sigs,
+            const_is_obj: self.const_is_obj,
+        }
     }
 }
