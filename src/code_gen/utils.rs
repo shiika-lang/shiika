67e48f090792b4cfb8aa243dd9ff326440aba75a
Yutaka HARA <yutaka.hara+github@gmail.com>
2021-01-17 19:19:09 +0900

    feat: Better panic message

--- src/code_gen/utils.rs
+++ src/code_gen/utils.rs
@@ -88,7 +88,12 @@ impl<'hir, 'run, 'ictx> CodeGen<'hir, 'run, 'ictx> {
                 idx as u32,
                 &format!("addr_{}", name),
             )
-            .unwrap_or_else(|_| panic!("build_llvm_struct_ref: elem not found (idx: {}, name: {})", &idx, &name));
+            .unwrap_or_else(|_| {
+                panic!(
+                    "build_llvm_struct_ref: elem not found (idx: {}, name: {}, object: {:?})",
+                    &idx, &name, &object
+                )
+            });
         self.builder.build_load(ptr, name)
     }
 
@@ -107,7 +112,12 @@ impl<'hir, 'run, 'ictx> CodeGen<'hir, 'run, 'ictx> {
                 idx as u32,
                 &format!("addr_{}", name),
             )
-            .unwrap();
+            .unwrap_or_else(|_| {
+                panic!(
+                    "build_llvm_struct_ref: elem not found (idx: {}, name: {}, object: {:?})",
+                    &idx, &name, &object
+                )
+            });
         self.builder.build_store(ptr, value);
     }
 
