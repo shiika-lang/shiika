018642ad083430147ddc761b45791de259b760c1
Yutaka HARA <yutaka.hara+github@gmail.com>
2021-07-08 10:31:31 +0900

    feat: exit(1) on error

--- src/main.rs
+++ src/main.rs
@@ -6,46 +6,32 @@ extern crate clap;
 fn main() {
     env_logger::init();
 
+    main_().unwrap_or_else(|err| {
+        print_err(err);
+        std::process::exit(1);
+    })
+}
+
+fn main_() -> Result<(), Error> {
     let yaml = load_yaml!("cli.yml");
     let matches = clap::App::from(yaml).get_matches();
 
     if let Some(ref matches) = matches.subcommand_matches("compile") {
         let filepath = matches.value_of("INPUT").unwrap();
-        compile(filepath);
+        runner::compile(filepath)?;
     }
 
     if let Some(ref matches) = matches.subcommand_matches("run") {
         let filepath = matches.value_of("INPUT").unwrap();
-        if compile(filepath) {
-            run(filepath);
-        }
+        runner::compile(filepath)?;
+        runner::run(filepath)?;
     }
 
     if matches.subcommand_matches("build_corelib").is_some() {
-        build_corelib();
+        runner::build_corelib()?;
     }
-}
 
-fn compile(filepath: &str) -> bool {
-    match runner::compile(filepath) {
-        Ok(_) => true,
-        Err(err) => {
-            print_err(err);
-            false
-        }
-    }
-}
-
-fn run(filepath: &str) {
-    runner::run(filepath).unwrap_or_else(|err| {
-        print_err(err);
-    });
-}
-
-fn build_corelib() {
-    runner::build_corelib().unwrap_or_else(|err| {
-        print_err(err);
-    });
+    Ok(())
 }
 
 fn print_err(err: Error) {
