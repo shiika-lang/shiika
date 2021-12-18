366be70badfd030b50dbe74e6677d467c4e3140e
Yutaka HARA <yutaka.hara+github@gmail.com>
2021-12-18 17:13:54 +0900

    refactor: Better error msg

--- lib/skc_codegen/src/utils.rs
+++ lib/skc_codegen/src/utils.rs
@@ -162,7 +162,7 @@ impl<'hir, 'run, 'ictx> CodeGen<'hir, 'run, 'ictx> {
             )
             .unwrap_or_else(|_| {
                 panic!(
-                    "build_llvm_struct_ref: elem not found (idx: {}, name: {}, object: {:?})",
+                    "build_llvm_struct_ref: elem not found (idx in struct: {}, register name: {}, object: {:?})",
                     &idx, &name, &object
                 )
             });
