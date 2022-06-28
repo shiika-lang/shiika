300017114ccad49621486be4b0dc94485325e00d
Yutaka HARA <yutaka.hara+github@gmail.com>
2022-06-28 16:32:41 +0900

    refactor: Rename class_dict to c

--- lib/skc_ast2hir/src/type_system/subtyping.rs
+++ lib/skc_ast2hir/src/type_system/subtyping.rs
@@ -4,7 +4,7 @@ use shiika_core::{ty, ty::*};
 /// Return true if `ty1` conforms to `ty2` i.e.
 /// an object of the type `ty1` is included in the set of objects represented by the type `ty2`
 #[allow(clippy::if_same_then_else)]
-pub fn conforms(class_dict: &ClassDict, ty1: &TermTy, ty2: &TermTy) -> bool {
+pub fn conforms(c: &ClassDict, ty1: &TermTy, ty2: &TermTy) -> bool {
     // `Never` is bottom type (i.e. subclass of any class)
     if ty1.is_never_type() {
         true
@@ -15,14 +15,14 @@ pub fn conforms(class_dict: &ClassDict, ty1: &TermTy, ty2: &TermTy) -> bool {
             ref1.upper_bound == ref2.upper_bound && ref1.lower_bound == ref2.lower_bound
         } else {
             let u1 = ref1.upper_bound.to_term_ty();
-            conforms(class_dict, &u1, ty2)
+            conforms(c, &u1, ty2)
         }
     } else if let TyBody::TyPara(ref2) = &ty2.body {
         if let TyBody::TyPara(ref1) = &ty1.body {
             ref1.upper_bound == ref2.upper_bound && ref1.lower_bound == ref2.lower_bound
         } else {
             let u2 = ref2.upper_bound.to_term_ty();
-            conforms(class_dict, ty1, &u2)
+            conforms(c, ty1, &u2)
         }
     } else {
         let is_void_fn = if let Some(ret_ty) = ty2.fn_x_info() {
@@ -30,7 +30,7 @@ pub fn conforms(class_dict: &ClassDict, ty1: &TermTy, ty2: &TermTy) -> bool {
         } else {
             false
         };
-        if let Some(t1) = ancestor_types(class_dict, ty1)
+        if let Some(t1) = ancestor_types(c, ty1)
             .iter()
             .find(|t| t.same_base(ty2))
         {
@@ -52,14 +52,14 @@ pub fn conforms(class_dict: &ClassDict, ty1: &TermTy, ty2: &TermTy) -> bool {
 /// top type. However, returns `Some(Object)` when either of the arguments
 /// is `Object`.
 pub fn nearest_common_ancestor(
-    class_dict: &ClassDict,
+    c: &ClassDict,
     ty1: &TermTy,
     ty2: &TermTy,
 ) -> Option<TermTy> {
     if ty1 == ty2 {
         return Some(ty1.clone());
     }
-    let t = _nearest_common_ancestor(class_dict, ty1, ty2);
+    let t = _nearest_common_ancestor(c, ty1, ty2);
     let obj = ty::raw("Object");
     if t == obj {
         if *ty1 == obj || *ty2 == obj {
@@ -74,11 +74,11 @@ pub fn nearest_common_ancestor(
 }
 
 /// Find common ancestor of two types
-fn _nearest_common_ancestor(class_dict: &ClassDict, ty1_: &TermTy, ty2_: &TermTy) -> TermTy {
+fn _nearest_common_ancestor(c: &ClassDict, ty1_: &TermTy, ty2_: &TermTy) -> TermTy {
     let ty1 = ty1_.upper_bound().into_term_ty();
     let ty2 = ty2_.upper_bound().into_term_ty();
-    let ancestors1 = ancestor_types(class_dict, &ty1);
-    let ancestors2 = ancestor_types(class_dict, &ty2);
+    let ancestors1 = ancestor_types(c, &ty1);
+    let ancestors2 = ancestor_types(c, &ty2);
     for t2 in &ancestors2 {
         let mut t = None;
         for t1 in &ancestors1 {
@@ -86,10 +86,10 @@ fn _nearest_common_ancestor(class_dict: &ClassDict, ty1_: &TermTy, ty2_: &TermTy
                 t = Some(t1);
                 break;
             } else if t1.same_base(t2) {
-                if conforms(class_dict, t1, t2) {
+                if conforms(c, t1, t2) {
                     t = Some(t2);
                     break;
-                } else if conforms(class_dict, t2, t1) {
+                } else if conforms(c, t2, t1) {
                     t = Some(t1);
                     break;
                 }
