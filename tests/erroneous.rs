4f00d21e3828644d71a65f7a947da7f380973de5
Yutaka Hara <yutaka.hara+github@gmail.com>
2023-10-04 16:34:33 +0900

    Disable coloring while erroneous test

--- tests/erroneous.rs
+++ tests/erroneous.rs
@@ -1,18 +1,17 @@
 use anyhow::Result;
+use concolor;
 use insta::{assert_snapshot, glob};
 use shiika::runner;
-use std::path::Path;
 
 #[test]
 fn test_erroneous() -> Result<()> {
-    let base = Path::new("./tests/erroneous/").canonicalize()?;
+    concolor::set(concolor::ColorChoice::Never);
     glob!("erroneous/**/*.sk", |sk_path| {
-        let result = match runner::compile(sk_path) {
+        let compiler_output = match runner::compile(sk_path) {
             Ok(_) => "".to_string(),
             Err(comp_err) => comp_err.to_string(),
         };
-        let rel_path = sk_path.strip_prefix(&base).unwrap();
-        assert_snapshot!(result);
+        assert_snapshot!(compiler_output);
     });
     Ok(())
 }
