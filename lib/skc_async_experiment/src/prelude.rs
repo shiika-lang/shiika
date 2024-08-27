5304f38601c347d8be8d6f13851382e109c5530d
Yutaka HARA <yutaka.hara+github@gmail.com>
2024-08-27 23:28:54 +0900

    fix arg syntax

--- lib/skc_async_experiment/src/prelude.rs
+++ lib/skc_async_experiment/src/prelude.rs
@@ -1,7 +1,7 @@
 /// Returns the functions needed to run the Milika program.
 pub fn prelude_funcs(main_is_async: bool) -> String {
     let main_sig = if main_is_async {
-        "requirement __internal__chiika_main(ENV env, FN((ENV, Int)->FUTURE) cont) -> FUTURE"
+        "requirement __internal__chiika_main(env: ENV, cont: Fn2<ENV, Int, FUTURE>) -> FUTURE"
     } else {
         "requirement __internal__chiika_main() -> Int"
     };
@@ -14,13 +14,13 @@ pub fn prelude_funcs(main_is_async: bool) -> String {
         + "class Main\n"
         + main_sig
         + "
-        requirement chiika_env_push_frame(ENV env, Int n) -> Null
-        requirement chiika_env_set(ENV env, Int idx, ANY obj, Int type_id) -> Null
-        requirement chiika_env_pop_frame(ENV env, Int expected_len) -> ANY
-        requirement chiika_env_ref(ENV env, Int idx, Int expected_type_id) -> Int
-        requirement chiika_spawn(FN((ENV,FN((ENV,Null)->FUTURE))->FUTURE) f) -> Null
-        requirement chiika_start_tokio(Int n) -> Int
-        def self.chiika_start_user(ENV env, FN((ENV,Int)->FUTURE) cont) -> FUTURE
+        requirement chiika_env_push_frame(env: ENV, n: Int) -> Null
+        requirement chiika_env_set(env: ENV, idx: Int, obj: ANY, type_id: Int) -> Null
+        requirement chiika_env_pop_frame(env: ENV, expected_len: Int) -> ANY
+        requirement chiika_env_get(env: ENV, idx: Int, expected_type_id: Int) -> ANY
+        requirement chiika_spawn(f: Fn2<ENV,Fn2<ENV,Null,FUTURE>,FUTURE>) -> Null
+        requirement chiika_start_tokio(n: Int) -> Int
+        def self.chiika_start_user(env: ENV, cont: Fn2<ENV,Int,FUTURE>) -> FUTURE
     " + call_user_main
         + "
         end
