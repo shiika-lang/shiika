db4eedebaa6d3622bec2426d007ae8c4cc745e4f
Yutaka HARA <yutaka.hara+github@gmail.com>
2024-12-31 18:55:47 +0900

    Cleanup comment

--- lib/skc_async_experiment/src/hir/untyped.rs
+++ lib/skc_async_experiment/src/hir/untyped.rs
@@ -243,11 +243,6 @@ fn compile_ty(n: &shiika_ast::UnresolvedTypeName) -> Result<hir::Ty> {
     let t = if n.args.len() == 0 {
         let s = n.names.join("::");
         match &s[..] {
-            // Internally used types (in src/prelude.rs)
-            //            "Shiika::Internal::Int64" => hir::Ty::Int64,
-            //            "ANY" => hir::Ty::Any,
-            //            "ENV" => hir::Ty::ChiikaEnv,
-            //            "FUTURE" => hir::Ty::RustFuture,
             _ => hir::Ty::raw(s),
         }
     } else {
