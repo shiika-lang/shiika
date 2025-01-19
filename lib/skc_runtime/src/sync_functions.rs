91fcb7bccb27ca2588a695e279ced668cb7e8364
Yutaka HARA <yutaka.hara+github@gmail.com>
2025-01-20 00:13:03 +0900

    skc_runtime: Add receiver arg

--- lib/skc_runtime/src/sync_functions.rs
+++ lib/skc_runtime/src/sync_functions.rs
@@ -1,7 +1,7 @@
 use shiika_ffi::core_class::SkInt;
 use shiika_ffi_macro::shiika_method;
 
-#[shiika_method("print")]
-pub extern "C" fn print(n: SkInt) {
+#[shiika_method("Object#print")]
+pub extern "C" fn print(_receiver: SkInt, n: SkInt) {
     println!("{}", n.val());
 }
