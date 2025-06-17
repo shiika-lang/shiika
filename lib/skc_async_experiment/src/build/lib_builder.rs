8a582d56b5f3e61912ed3156d4a5ed91e317f2d0
Yutaka HARA <yutaka.hara+github@gmail.com>
2025-06-17 21:39:59 +0900

    fix warning

--- lib/skc_async_experiment/src/build/lib_builder.rs
+++ lib/skc_async_experiment/src/build/lib_builder.rs
@@ -1,11 +1,9 @@
 //! Compiles the Shiika code in a package into single .bc.
 use crate::build;
 use crate::cli::Cli;
-use crate::hir;
 use crate::mir;
 use crate::package::Package;
 use anyhow::Result;
-use skc_hir::MethodSignature;
 use skc_mir::LibraryExports;
 use std::collections::HashMap;
 use std::io::Write;
