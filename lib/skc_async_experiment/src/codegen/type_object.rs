3d5a2238a860315e59102c2c54b42e2ed27afe43
Yutaka HARA <yutaka.hara+github@gmail.com>
2025-10-24 22:31:54 +0900

    Create meta type obj

--- lib/skc_async_experiment/src/codegen/type_object.rs
+++ lib/skc_async_experiment/src/codegen/type_object.rs
@@ -7,16 +7,23 @@ use crate::prelude;
 use shiika_core::ty::TermTy;
 
 pub fn create<'run>(gen: &mut CodeGen<'run, '_>, the_ty: &TermTy) -> SkObj<'run> {
-    // REFACTOR: can we do this in MIR level?
-    // this is implemented in codegen because HirClassLiteral appears on the rhs of const
-    // assignment and mir::async_splitter cannot handle `Exprs` in inner position.
-
     debug_assert!(!the_ty.fullname.is_meta());
     let type_obj = create_obj(gen, the_ty);
 
     if the_ty.fullname.0 == "Metaclass" {
         // Overwrite .class to achieve `Metaclass.class == Metaclass`.
         instance::set_class_obj(gen, &type_obj, SkClassObj(type_obj.0));
+    } else {
+        let meta_type_obj = {
+            let o = create_obj(gen, &the_ty.meta_ty());
+            let the_metaclass = gen
+                .compile_constref("::Metaclass")
+                .expect("Metaclass class object not found")
+                .into_pointer_value();
+            instance::set_class_obj(gen, &o, SkClassObj(the_metaclass));
+            o
+        };
+        instance::set_class_obj(gen, &type_obj, SkClassObj(meta_type_obj.0));
     }
 
     type_obj
@@ -25,7 +32,14 @@ pub fn create<'run>(gen: &mut CodeGen<'run, '_>, the_ty: &TermTy) -> SkObj<'run>
 /// Create a type object
 fn create_obj<'run>(gen: &mut CodeGen<'run, '_>, the_ty: &TermTy) -> SkObj<'run> {
     let name_str = string_literal::generate(gen, &the_ty.fullname.0);
-    let cls_obj = instance::allocate_sk_obj(gen, "Class");
+    let cls_obj = instance::allocate_sk_obj(
+        gen,
+        if the_ty.is_metaclass() {
+            "Metaclass"
+        } else {
+            "Class"
+        },
+    );
     instance::build_ivar_store_raw(
         gen,
         cls_obj.clone(),
