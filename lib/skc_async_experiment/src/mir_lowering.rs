1794cc04a0f8cc216dc9a836b708f7411aea938e
Yutaka HARA <yutaka.hara+github@gmail.com>
2026-02-08 22:01:09 +0900

    mir: Reimpl VTableRef
    re-enabled vtable related checks in the verifier

--- lib/skc_async_experiment/src/mir_lowering.rs
+++ lib/skc_async_experiment/src/mir_lowering.rs
@@ -2,6 +2,7 @@ pub mod async_splitter;
 pub mod asyncness_check;
 pub mod insert_allocs;
 pub mod let_bind_async;
+pub mod lower_vtable_ref;
 pub mod pass_async_env;
 pub mod resolve_env_op;
 pub mod splice_exprs;
