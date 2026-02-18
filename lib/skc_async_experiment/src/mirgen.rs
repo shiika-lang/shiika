16f3181e3e6d370b4b95199b79196eb47e9fda58
Yutaka Hara <yutaka.hara+github@gmail.com>
2026-02-18 16:58:42 +0900

    fix merging error

--- lib/skc_async_experiment/src/mirgen.rs
+++ lib/skc_async_experiment/src/mirgen.rs
@@ -540,6 +540,7 @@ impl<'a> Compiler<'a> {
             ret_ty: convert_ty(ret_ty.clone()),
             body_stmts: final_body,
             sig: None,
+            lvar_count: None,
         }
     }
 
