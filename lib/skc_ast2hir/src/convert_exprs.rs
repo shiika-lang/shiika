6060dd8482268d79fc95301534bbb1bd5946b0e1
Yutaka Hara <yutaka.hara+github@gmail.com>
2026-04-17 18:06:21 +0900

    improve error msg

--- lib/skc_ast2hir/src/convert_exprs.rs
+++ lib/skc_ast2hir/src/convert_exprs.rs
@@ -872,10 +872,11 @@ impl<'hir_maker> HirMaker<'hir_maker> {
                 return Ok(Hir::const_ref(ty, full, locs.clone()));
             }
         }
-        Err(error::program_error(&format!(
-            "constant `{:?}' was not found",
-            name.0.join("::")
-        )))
+        let main_msg = format!("constant `{}' was not found", name.0.join("::"));
+        let report = skc_error::build_report(main_msg, locs, |r, locs_span| {
+            r.with_label(skc_error::Label::new(locs_span))
+        });
+        Err(error::program_error(report))
     }
 
     /// Get the value of a class-wise or method-wise type argument.
